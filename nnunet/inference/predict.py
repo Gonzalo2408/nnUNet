8f0811649221eaf0b49bf72bf0d0e1b5a96adf84
Gonzalo2408 <gonzalore1999@gmail.com>
2023-06-07 16:30:06 +0200

    Get arch

--- nnunet/inference/predict.py
+++ nnunet/inference/predict.py
@@ -250,17 +250,17 @@ def predict_cases(model, list_of_lists, output_filenames, folds, save_npz, num_t
 
                     for k in range(len(model_children[i][j])):
 
-                        print(model_children[i][j][k])
+                        print(model_children[i][j][k].children())
 
-                        for l in range(len(model_children[i][j][k])):
+                        # for l in range(len(model_children[i][j][k])):
 
-                            print(type(model_children[i][j][k][l]))
+                        #     print(type(model_children[i][j][k][l]))
 
-                            if type(model_children[i][j][k][l]) == torch.nn.Conv2d:
+                        #     if type(model_children[i][j][k][l]) == torch.nn.Conv2d:
 
-                                counter += 1
-                                model_weights.append(model_children[i][j][k][l].weight)
-                                conv_layers.append(model_children[i][j][k][l])
+                        #         counter += 1
+                        #         model_weights.append(model_children[i][j][k][l].weight)
+                        #         conv_layers.append(model_children[i][j][k][l])
                 
 
             # if type(model_children[i]) == torch.nn.Conv2d:
